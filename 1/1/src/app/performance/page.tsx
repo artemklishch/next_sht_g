5d67d08bc9976f174a8c32ed9b38fd0d9d6f1d41
Artem Klishch <artemklishch@gmail.com>
2023-12-25 21:22:59 +0200

    fasdfasdf

--- 1/1/src/app/performance/page.tsx
+++ 1/1/src/app/performance/page.tsx
@@ -1,3 +1,8 @@
+import Hero from "@/components/hero"
+import performanceImg from 'public/performance.jpg'
+
 export default function PerformancePage() {
-    return <div>Performance Page</div>
+    return (
+        <Hero imgData={performanceImg} imgAlt="welding" title="We serve hight performance applications" />
+    )
 }
---
+++ 2mutations/1/src/db/index.ts
@@ -0,0 +1,13 @@
+// import { PrismaClient } from '@prisma/client'
+
+// export const db = new PrismaClient()
+
+// db.snippet.create({
+//     data: {
+//         title: 'Title!',
+//         code: 'const abc = () => {}',
+//     }
+// })
+import { PrismaClient } from '@prisma/client';
+
+export const db = new PrismaClient();
