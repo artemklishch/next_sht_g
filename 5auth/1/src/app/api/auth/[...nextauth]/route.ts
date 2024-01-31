ac634c9a13177fa679e4fdf4b17b25385f153c08
artemklishch <artemklishch@gmail.com>
2024-01-31 08:48:27 +0200

    afgsdfg

---
+++ 5auth/1/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,7 @@
+// export async function GET() {}
+
+// export async function POST() {}
+
+// above the functions we can make to wrapp logic with the GET/POST methods
+
+export { GET, POST } from "@/auth";
