91f9dec283674273f993ddf0af801fedf56fde9f
artemklishch <artemklishch@gmail.com>
2024-02-03 17:59:01 +0200

    fgsdfg

--- 5auth/3/src/actions/create-topic.ts
+++ 5auth/3/src/actions/create-topic.ts
@@ -1,5 +1,68 @@
 "use server";
 
-export async function createTopic() {
-  // TODO: revalidate the homepage
+import { revalidatePath } from "next/cache";
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase lettersor dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  // const name = formData.get("name");
+  // const description = formData.get("description");
+  // console.log(name, description);
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    // console.log("error", result.error.flatten().fieldErrors);
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return { errors: { _form: ["You must be signed in to do this."] } };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { errors: { _form: [err.message] } };
+    } else {
+      return { errors: { _form: ["Something went wrong"] } };
+    }
+  }
+  revalidatePath("/");
+  redirect(paths.topicShowPath(topic.slug));
 }
