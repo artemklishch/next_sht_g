a665e7014abf3f51b08df3d326b0bb153ef249d0
artemklishch <artemklishch@gmail.com>
2024-02-03 20:00:34 +0200

    dsfgdfsg

---
+++ 6dbqueries/1/src/app/page.tsx
@@ -0,0 +1,19 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
+import TopicsList from "@/components/topics/topics-list";
+import { Divider } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl 02">Top Posts</h1>
+      </div>
+      <div className="order shadow py-3 px-2">
+        <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicsList />
+      </div>
+    </div>
+  );
+}
